d703fd10980dc8f202b3b71f945a34eb1527f6d0
Hugo Youf <91455667+HugoYOUF@users.noreply.github.com>
2024-11-15 13:57:34 +0100

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probleme_Hugo_Youf_Terence_Roumilhac_TD_E
+{
+    internal class Dictionnaire
+    {
+    }
+}
